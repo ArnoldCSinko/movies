84380feed1fc21172c3a9c1fe4b3024246fef895
Chris Sinko <arnoldcsinko@gmail.com>
2017-10-08 20:14:24 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const path          = require('path');
+const http          = require('http');
+const app           = express();
+
+// API for interacting with MongoDB
+const api           = require('./server/routes/api');
+
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output file
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API Location
+app.use('/api', api);
+
+// Send all other requests to Angular
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`running on localhost: ${port}`));
+
+
+
